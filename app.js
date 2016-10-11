56a2fdbf53cdd30b075d172f5ddbb5c48ae07e5f
Russell Schmidt <russell.schmidt@me.com>
2016-10-10 20:00:57 -0700

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
 // Setting up libraries and configuration
-var express = require('express');		// The require() function includes the code for Express
-var app = express();					// Initialize the Express library
-var http = require('http').Server(app);	// Initialize an HTTP server
-var io = require('socket.io')(http);	// Include and initialize SocketIO
-var port = process.env.PORT || 8000;	// Set the default port number to 8000, or use Heroku's settings (process.env.PORT)
+var express = require('express');       // The require() function includes the code for Express
+var app = express();                    // Initialize the Express library
+var http = require('http').Server(app); // Initialize an HTTP server
+var io = require('socket.io')(http);    // Include and initialize SocketIO
+var port = process.env.PORT || 8000;    // Set the default port number to 8000, or use Heroku's settings (process.env.PORT)
 
-// Use Express to serve everything in the "public" folder as static files
+                                        // Use Express to serve everything in the "public" folder as static files
 app.use(express.static('public'));
 
-// Activate the server and listen on our specified port number
+                                        // Activate the server and listen on our specified port number
 http.listen(port, function() {
-	// Display this message in the server console once the server is active
-	console.log('Listening on port ' + port);
+                                        // Display this message in the server console once the server is active
+  console.log('Listening on port ' + port);
 
 });
 
+
+// PART ONE
 // When a user connects over websocket,
+// io.on('connection', function(socket) {
+
+  // // Display this message in the server console
+  // console.log('A user connected!');
+
+  // // var obj = {test: "test from server", rando: 1230123};
+  // // Send an event named "test" to every client with io.sockets.emit() function (or just io.emit() for short)
+  // // and with this event, send the string 'Hey everyone...' as the data
+  // // io.sockets.emit('test', obj);
+
+
+
+  // // When the server receives an event named "test",
+  // socket.on('test', function(data) {
+  //   // Take whatever data was received and display it in the server console
+  //   console.log(data);
+
+  //   socket.emit('test', "I SAW YOU CLICK ");
+  //   io.sockets.emit('test', data)
+
+  //   socket.broadcast.emit('test', 'OFF WITH THEIR HEAD');
+  // });
+// }); // End of SocketIO code
+
+
+// PART TWO
 io.on('connection', function(socket) {
 
-	// Display this message in the server console
-	console.log('A user connected!');
+  socket.on('mousedown', function(data) {
+    console.log(data);
+    socket.broadcast.emit('mousedown', data);
+  });
+
+  socket.on('mousemove', function(data) {
+    console.log(data);
+    socket.broadcast.emit('mousemove', data);
+  });
+
+});
 
 
-
-});	// End of SocketIO code
--- public/local.js
+++ public/local.js
@@ -1,3 +1,130 @@
+// // Start a WebSocket connection with the server using SocketIO
+// var socket = io();
+
+// var num = Math.random()*10000
+// var obj = {client:"I am the client", rando: num};
+
+
+// // Send an event named "test" to the server with socket.emit() function
+// // and with this event, send the string 'Hi, this is...' as the data
+// // // socket.emit('test', 'Hello from the client!');
+// socket.emit('test', obj);
+
+// // When the client receives an event named "test",
+// socket.on('test', function(data) {
+//   // Take whatever data was received and display it in the client console
+//   console.log(data);
+// });
+
+// document.addEventListener('click', function(event) {
+//   console.log(event);
+
+//   obj = {x: event.clientX, y: event.clientY}
+
+//   socket.emit('test', obj);
+// });
+
+//   PART TWO 
+// // Create a variable for the web page's canvas element, which has id="mycanvas":
+// var canvas = document.getElementById('mycanvas');
+
+// // Create a variable to access the two-dimensional canvas drawing functions
+// var pen = canvas.getContext('2d');   // this creates an object we arbitrarily call pen
+// // you can also call it context. We can set up the .getContext as 3d too
+
+// pen.beginPath();  // Initialize
+// pen.moveTo(0,0);  // Choose starting coordinates (top left corner of canvas)
+// pen.lineTo(200,500);  // Choose ending coordinates (500 pixels from top, 500 pixels from left)
+// pen.lineTo(400,0);
+// pen.lineTo(600,500);
+// pen.lineTo(800,0);
+// pen.lineTo(1000,500);
+
+// pen.moveTo(0,250);
+// pen.lineTo(1000,250);
+// pen.moveTo(0, 500);
+// pen.lineTo(1000, 500);
+// pen.stroke();   // Draw the line
+
+// PART THREE
 // Start a WebSocket connection with the server using SocketIO
-var socket = io(); 	// Note that the SocketIO client-side library was imported on line 13 of index.html,
-					// and this file (local.js) was imported on line 14 of index.html
+var socket = io();
+
+// Create a variable for the web page's canvas element, which has id="mycanvas"
+var canvas = document.getElementById('mycanvas');
+
+// Create a variable to access the two-dimensional canvas drawing functions
+var pen = canvas.getContext('2d');
+
+// Set event listeners for when the mouse button is pressed down, when the mouse moves, and when the mouse button is released
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', drawStuff);
+canvas.addEventListener('mouseup', stopDrawing);
+
+// function startDrawing(event) {
+//   console.log("START: " + event.clientX + ", " + event.clientY);
+
+//   // Which canvas drawing functions should go here??
+//   // HINT: start with pen.beginPath();
+//   pen.beginPath();
+//   pen.moveTo(event.clientX, event.clientY);
+// }
+
+// function drawStuff(event) {
+//   console.log("Moved to: " + event.clientX + ", " + event.clientY);
+
+//   // Which canvas drawing functions should go here?? (or none at all?)
+//   pen.lineTo(event.clientX, event.clientY);
+//   // pen.stroke();
+// }
+
+// function stopDrawing(event) {
+//   console.log("STOP: " + event.clientX + ", " + event.clientY);
+
+//   // Which canvas drawing functions should go here?? (or none at all?)
+//   pen.stroke();
+// }
+
+// PART FOUR
+// for Drawing by client
+var isDrawing = false;
+
+function startDrawing(event) {
+  console.log("START: " + event.clientX + ", " + event.clientY);
+  isDrawing = true;
+  pen.beginPath();
+  pen.moveTo(event.clientX, event.clientY );
+  // note JavaScript same function name 'mousedown' - doesnt HAVE to match JS
+  // just 
+  socket.emit('mousedown', {x: event.clientX, y: event.clientY}); 
+}
+
+// for Drawing by Client
+function drawStuff(event) {
+  console.log("Moved to: " + event.clientX + ", " + event.clientY);
+  if (isDrawing) {
+    pen.lineTo(event.clientX, event.clientY);
+    pen.stroke();
+    socket.emit('mousemove', {x: event.clientX, y: event.clientY});
+  }
+}
+
+function stopDrawing(event) {
+  console.log("STOP: " + event.clientX + ", " + event.clientY);
+
+  // Which canvas drawing functions should go here?? (or none at all?)
+  pen.stroke();
+}
+
+// to receive the client data and draw it on the canvas
+// Add this to the bottom of your local.js file:
+socket.on('mousedown', function(data) {
+  pen.beginPath();
+  pen.moveTo(data.x, data.y);
+});
+
+socket.on('mousemove', function(data) {
+  pen.lineTo(data.x, data.y);
+  pen.stroke();
+});
+
